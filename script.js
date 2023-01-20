ff91729d42b21b0193558fff65d1c520e85b39d6
Jamie Clark <clarkcjamie@gmail.com>
2023-01-20 00:57:28 -0500

    add gridbox and its css properties into JS DOM, add over 100x100 error

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-const mainContainer = document.querySelector('.gridbox');
-// create empty p tag to display result of round
+const rightPanel = document.querySelector('.right-panel');
+const gridbox = document.querySelector('.gridbox');
+// setting up properties of the huge gridbox inside the DOM so that I can grab the width from within!!!!!
+gridbox.style.cssText = "box-sizing: content-box; width: 500px; height: 500px; border: 1px solid black; display: flex; flex-wrap: wrap;"
+// actually grabbing the css width property I set to the grid so that I can use it as a variable in case it ever changes!
+// using regex variable to grab everything before 'px' in the width property!
+const regex = /(.*)px/gm;
+// creating the constant that holds the integer value of the css value of '500px' for instance (stores the integer value '500')
+const gridboxWidth = parseInt(gridbox.style.width.match(regex));
 
-
-let i = 0;
-let userInput = 16;
+// NOTE: THIS LINE (let userInput = 30;) IS THE ONLY LINE THE USER WILL BE ABLE TO CHANGE. This is the only variable the user will be able to change in the future!
+// NOTE: DO NOT exceed 100 x 100 px grid please!!!!
+let userInput = 30;
 let squareUserInput = (userInput ** 2);
+// this always calculates the exact pixels needed for the grid so that the only thing the user needs to change is the user input (ex: 16 x 16) dimension!
+// NOTE: ALL other variables are related back / dependent on / tied to what the user sets as the input for the grid. Variables like smallGridWidth and 
+// BELOW: is the calculation variable to obtain the grid square width based on the current large gridbox container width and height!!!
+const smallGridWidth = (gridboxWidth / userInput);
+// I am only using .toFixed(2) for display purposes in the console log! I do NOT want to round my actual variable down at all!
+
+console.log(`The current dimension of the individual small grid square is ${smallGridWidth.toFixed(2)} x ${smallGridWidth.toFixed(2)} pixels.`);
+console.log (`The main gridbox dimension is ${gridboxWidth} x ${gridboxWidth} px with a ${userInput} x ${userInput} px size grid within.`)
 
 
 for (let i = 0; i < squareUserInput; i++) {
-    const gridTest= document.createElement('div');
+    if (userInput <= 100) {
+        const smallGrid= document.createElement('div');
+        // create a classname for each div
+        smallGrid.className = 'mini-grid-square';
+        // width and height is determined directly from the user's input of grid size!
+        smallGrid.style.width = `${smallGridWidth}px`;
+        // easily setting height below to match that width pixel dimension so that grid squares always come out uniform.
+        smallGrid.style.height = smallGrid.style.width;
+        // place div element inside #result div
+        gridbox.appendChild(smallGrid);
+    }
+    else {
+        console.log('PLEASE INSERT A GRID VALUE LESS THAN OR EQUAL TO 100 x 100 PIXELS!')
+        //break allows you to only run this command once rather than a million times
+        break;
+    }
 
-    // create a classname for each div
-    gridTest.className = 'mini-grid-square';
-    // default text for this div when page loads up
-    // place div element inside #result div
-    mainContainer.appendChild(gridTest);
   }
 
